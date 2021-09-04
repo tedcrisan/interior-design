eb590baee8c2aab0e9708e501576fd44c9b70861
Ted Crisan <tedcrisan@live.ca>
2021-09-04 15:34:55 -0400

    Fix crossOrigin typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Designed to be Different" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Montserrat&display=swap"
           rel="stylesheet"
